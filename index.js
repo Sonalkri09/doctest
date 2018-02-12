d55efa2aa560693c98378e17598fbe38a84f5bfc
Sonalkri09 <ninthjune415@gmail.com>
2018-02-12 15:00:29 +0530

    codedone

--- index.js
+++ index.js
@@ -1,16 +1,104 @@
-<!DOCTYPE HTML>
-<html>
+var fs = require('fs');
+var file = 'doc.txt';
+var file1 = 'doc1.txt'
+fs.readFile(file1, 'utf8', function (err, data) {
 
-<body>
+  if (err) throw err;
 
-  <p>Before the script...</p>
 
-  <script>
-    alert( 'Hello, world!' );
-  </script>
+  var wordsArray = splitByWords(data);
+  var wd = wordsArray.length;
+  console.log(wd);
 
-  <p>...After the script.</p>
+  sw = require('stopword')
+var taAr=sw.removeStopwords(wordsArray)
+console.log(taAr)
 
-</body>
+var wrdc = taAr.length;
+console.log(wrdc);
+spellcheck (taAr)
 
-</html>
+console.log("Success!!")
+
+
+poss1(taAr);
+
+  });
+
+
+fs.readFile(file, 'utf8', function (err, data) {
+
+  if (err) throw err;
+
+
+  var wordsArray = splitByWords(data);
+  var wd = wordsArray.length;
+  console.log(wd);
+
+  sw = require('stopword')
+var taAr=sw.removeStopwords(wordsArray)
+console.log(taAr)
+
+var wrdc = taAr.length;
+console.log(wrdc);
+spellcheck (taAr)
+
+console.log("Success!!")
+
+
+poss1(taAr);
+
+  });
+
+function splitByWords (text) {
+  // split string by spaces (including spaces, tabs, and newlines)
+  var wordsArray = text.split(/\s+/);
+  return wordsArray;
+  
+
+}
+
+function spellcheck (taAr){
+    const spell = require('spell-checker-js')
+    spell.load('en')
+    var i,mis=0;
+    for(i=0;i<taAr.length;i++)
+    {
+        var check = spell.check(taAr[i])
+        if (check != false) {
+            console.log(taAr[i]);
+            mis++;
+        }
+        
+    }
+    console.log(mis)
+    
+    return 0;
+}
+
+function poss1 (taAr){
+var WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
+    console.log(taAr);
+    var str= taAr.toString();
+    wordpos.getPOS(str, console.log)
+
+}
+// Load dictionary
+
+ 
+// Checking text
+
+/*let output={percentage :90%};
+
+let json = json.stringify(output);
+
+fs.writeFile('data.json',json,'utf8',(err)=> {
+    if (err) {
+        console.log("Error");
+        return;
+    }
+    console.log("Success");
+
+    });
+*/
