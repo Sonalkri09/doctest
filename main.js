a839a1ef2df68b8c6a0c818902a6679c9f084ffb
Sonal <ninthjune415@gmail.com>
2018-02-26 08:03:11 +0530

    changes in main.js

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 var readline=require('readline');
 var fs = require('fs');
-var file = 'nv.txt';
-var file1 = 'nv1.txt';
-fs.readFile(file1, 'utf8', function (err, data) {
+var file = 'doc.txt';
+var file1 = 'doc1.txt';
 
-  if (err) throw err;
+var data1 = fs.readFileSync(file1, 'utf8');
+
+wordsArray1 = splitByWords(data1);
+
+var wd1 = wordsArray1.length;
+console.log(wd1);
+var data = fs.readFileSync(file, 'utf8');
+wordsArray = splitByWords(data);
+
+var wd = wordsArray.length;
+console.log(wd);
+var dec=0;
+wcdec(wrdc,wrdc1);
+if(dec == 1)
+console.log("Document accepted");
+else console.log("Document rejected");
+
+sw1 = require('stopword')
+var taAr1=sw1.removeStopwords(wordsArray1)
+console.log(taAr1)
+
+var wrdc1 = taAr1.length;
+console.log(wrdc1);
+spellcheck (taAr1)
+
+console.log("Success!!")
 
 
-  var wordsArray = splitByWords(data);
-  var wd = wordsArray.length;
-  console.log(wd);
+poss1(taAr1);
 
-  sw = require('stopword')
+sw = require('stopword')
 var taAr=sw.removeStopwords(wordsArray)
 console.log(taAr)
 
@@ -24,32 +46,6 @@ console.log("Success!!")
 
 poss1(taAr);
 
-  });
-
-
-fs.readFile(file, 'utf8', function (err, data) {
-
-  if (err) throw err;
-
-
-  var wordsArray = splitByWords(data);
-  var wd = wordsArray.length;
-  console.log(wd);
-
-  sw = require('stopword')
-var taAr=sw.removeStopwords(wordsArray)
-console.log(taAr)
-
-var wrdc = taAr.length;
-console.log(wrdc);
-spellcheck (taAr)
-
-console.log("Success!!")
-
-
-poss1(taAr);
-
-  });
 
 function splitByWords (text) {
   // split string by spaces (including spaces, tabs, and newlines)
@@ -85,23 +81,124 @@ var WordPOS = require('wordpos'),
     wordpos.getPOS(str, console.log)
 
 }
-// Load dictionary
+
+
+
+function wcdec(wrdc,wrdc1)
+{
+	if((wrdc1-wrdc)>1000)
+	dec=0;
+	else dec=1;	
+}
+
+var WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
+var standard_token_array = new Array(4);
+var mydocument_token_array = new Array(4); 
+function x(document) {
+	return new Promise((resolve, reject) => {
+		wordpos.getNouns(document, function (result) {
+			standard_token_array[0] = result.length;
+		});
+
+		wordpos.getAdjectives(document, function (result) {
+			standard_token_array[1] = result.length;
+		});
+
+		wordpos.getVerbs(document, function (result) {
+			standard_token_array[2] = result.length;
+		});
+
+		wordpos.getAdverbs(document, function (result) {
+			standard_token_array[3] = result.length;
+			resolve("completed");
+		});
+
+		var n = 0;
+		if (n == 1) {
+			reject("failed");
+
+		}
+	});
+}
+
+
+
+x(data1).then((message) => {
+		function y(document) {
+			return new Promise((resolve, reject) => {
+				wordpos.getNouns(document, function (result) {
+					mydocument_token_array[0] = result.length;
+				});
+
+				wordpos.getAdjectives(document, function (result) {
+					mydocument_token_array[1] = result.length;
+				});
+
+				wordpos.getVerbs(document, function (result) {
+					mydocument_token_array[2] = result.length;
+				});
+
+				wordpos.getAdverbs(document, function (result) {
+					mydocument_token_array[3] = result.length;
+					resolve("completed");
+				});
+
+				var n = 0;
+				if (n == 1) {
+					reject("failed");
+
+				}
+			});
+		}
+
+
+
+		y(data).then((message) => {
+				//json_creation();
+			})
+			.catch((message) => {
+
+				console.log("Failed");
+			});
+
+
+	})
+	.catch((message) => {
+
+		console.log("Failed");
+	});
+
+
+
+
 
  
 // Checking text
 
-let obj=["b","a","c"];
-let doc = {};
-obj.map(state =>{
-	doc[state]={
-		wc: 10,
-		mis: 11,
+
+
+
+
+/*let fls =['base file','ind file'];
+
+let oup = {};
+
+fls.map(state=> {
+	oup[state]={
+		word_count :  ,
+		spell_mistakes :  ,
+
 	}
 });
-	
+let json = json.stringify(oup,null,2);
 
+fs.writeFile('data.json',json,'utf8',(err)=> {
+	if (err) {
+		console.log("Error");
+		return;
+	}
+	console.log("Success");
 
-let json = JSON.stringify(doc,null,2);
-
-
-fs.writeFile('data.json',json,'utf8',(err)=> {})
+	});
+/*
